1cf2da8e3984168da38b2ecbfdd4f769ad313b8e
Mizukiqwq <1139198820@qq.com>
2024-08-26 11:23:06 +0800

    Base Init of OpenMP

---
+++ BasicOpenMP.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+    //int thread_count = strtol(argv[1], NULL, 10);
+    omp_set_num_threads(8);
+    
+#pragma omp parallel 
+    {
+        int thread_count = omp_get_num_threads();
+        int my_rank = omp_get_thread_num();
+        printf("Hello from thread %d of %d\n", my_rank+1, thread_count);
+    }
+
+    return 0;
+}
